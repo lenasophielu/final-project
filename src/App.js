e3edbd6edc02f20b28508d6fa546a182f90919e1
lenasophielu <72163661+lenasophielu@users.noreply.github.com>
2021-03-05 17:18:38 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ export default function App() {
     <div className="App">
       Weather App
       <footer>
-        This project was coded by <a href="https://www.linkedin.com/in/lena-lumplecker-855b30137/" target="_blank">Lena Lumplecker</a> and is
-      <a href="https://github.com/lenasophielu/final-project" target="_blank"> open-sourced on Git-Hub</a>
+        This project was coded by <a href="https://www.linkedin.com/in/lena-lumplecker-855b30137/" >Lena Lumplecker</a> and is
+      <a href="https://github.com/lenasophielu/final-project" > open-sourced on Git-Hub</a>
       </footer>
     </div>
   
