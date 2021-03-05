fd098cd85d68c7cdb8d787f7655cf408db044229
lenasophielu <72163661+lenasophielu@users.noreply.github.com>
2021-03-05 17:04:27 +0100

    inserted footer with info

--- src/App.js
+++ src/App.js
@@ -5,7 +5,12 @@ export default function App() {
   return (
     <div className="App">
       Weather App
+      <footer>
+        This project was coded by <a href="https://www.linkedin.com/in/lena-lumplecker-855b30137/" target="_blank">Lena Lumplecker</a> and is
+      <a href="https://github.com/lenasophielu/final-project" target="_blank"> open-sourced on Git-Hub</a>
+      </footer>
     </div>
+  
   );
 }
 
