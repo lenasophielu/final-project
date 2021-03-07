5d32c3299d8e95981caed9a95328a36b5baee830
lenasophielu <72163661+lenasophielu@users.noreply.github.com>
2021-03-07 13:44:42 +0100

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       Weather App
       <footer>
         This project was coded by <a href="https://www.linkedin.com/in/lena-lumplecker-855b30137/" >Lena Lumplecker</a> and is
       <a href="https://github.com/lenasophielu/final-project" > open-sourced on Git-Hub</a>
       </footer>
+    </div>
     </div>
   
   );
