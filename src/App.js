7a5787f49132ceb36b4c6027263e0c29c3ae5ddf
lenasophielu <72163661+lenasophielu@users.noreply.github.com>
2021-03-07 20:57:29 +0100

    included axios & api

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import './App.css';
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-      Weather App
+     <Weather />
       <footer>
         This project was coded by <a href="https://www.linkedin.com/in/lena-lumplecker-855b30137/" >Lena Lumplecker</a> and is
       <a href="https://github.com/lenasophielu/final-project" > open-sourced on Git-Hub</a>
---
+++ src/Weather.js
@@ -0,0 +1,94 @@
+import React, {useState} from "react";
+import "./Weather.css";
+import axios from "axios";
+
+
+export default function Weather() {
+
+  const [weatherData, setWeatherData] = useState({ready: false});
+
+  function handleResponse(response) {
+    console.log(response.data);
+    setWeatherData({
+      ready: true,
+      temperature: response.data.main.temp,
+      humidity:response.data.main.humidity,
+      description: response.data.weather[0].description,
+      iconUrl: "https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png",
+      wind: response.data.wind.speed,
+      city: response.data.name,
+      date: "Monday, 8pm"
+    });
+  }
+
+
+if (weatherData.ready) {
+return (
+    <div className="App">
+      <div class="container">
+        <div class="weather-app">
+          <form class="search-form" id="search-form">
+            <div class="form-row">
+              <div class="col-5">
+                <input
+                  type="text"
+                  class="form-control"
+                  placeholder="Where?"
+                  id="city-input"
+              
+                />
+              </div>
+              <div class="col-1.5">
+                <input
+                  type="submit"
+                  value="Search"
+                  class="btn btn-info"
+                  id="searchbutton"
+                />
+              </div>
+
+              <button
+                type="button"
+                class="btn btn-secondary"
+                id="current-location-button"
+              >
+                Current
+              </button>
+            </div>{" "}
+          </form>
+        </div>
+
+        <div class="row row-cols-3">
+          <div id="city" class="city">{weatherData.city}</div>
+          <div class="today">
+            <img src={weatherData.iconUrl} alt={weatherData.description} id="icon" />
+          </div>
+          <div id="date" class="date">{weatherData.date}</div>
+          <div class="row"></div>
+        </div>
+        <div class="row row-cols-9">
+          <div id="temperature" class="temperature">{Math.round(weatherData.temperature)}</div>
+          <div class="unit">°C </div>
+        </div>
+        <span class="weatherDefinition">{weatherData.description}</span>
+        <ul class="conditions">
+          <li>
+           Humidity: {weatherData.humidity}: <span id="humidity"></span>%
+          </li>
+          <li>
+           Wind: {weatherData.wind} <span id="wind"></span> km/h
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+ }
+  else {
+  const apiKey = "643f59cd53a59997e40900d7df9a1d27";
+  let city = "London"
+  let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(handleResponse);
+return "Loading...";
+}
+  }
+
