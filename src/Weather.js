1ccbbee3d1c41aa7aaaa7c990a6f2bb1ea760184
lenasophielu <72163661+lenasophielu@users.noreply.github.com>
2021-03-08 09:27:54 +0100

    icon updated

--- src/Weather.js
+++ src/Weather.js
@@ -1,12 +1,13 @@
 import React, {useState} from "react";
 import "./Weather.css";
-import FormattedDate from "./FormattedDate";
+import WeatherInfo from "./WeatherInfo";
 import axios from "axios";
 
 
 export default function Weather(props) {
 
   const [weatherData, setWeatherData] = useState({ready: false});
+  const [city, setCity] = useState(props.defaultCity);
 
   function handleResponse(response) {
     console.log(response.data);
@@ -15,12 +16,27 @@ export default function Weather(props) {
       temperature: response.data.main.temp,
       humidity:response.data.main.humidity,
       description: response.data.weather[0].description,
-      iconUrl: "https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png",
+      iconUrl: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
       wind: response.data.wind.speed,
       city: response.data.name,
       date: new Date (response.data.dt * 1000)
     });
   }
+function search()
+{ const apiKey = "643f59cd53a59997e40900d7df9a1d27";
+  let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(handleResponse);
+
+}
+
+
+function handleSubmit(event){
+  event.preventDefault();
+search();}
+
+function handleCityChange(event) {
+  setCity(event.target.value);
+}
 
 
 if (weatherData.ready) {
@@ -28,7 +44,8 @@ return (
     <div className="App">
       <div class="container">
         <div class="weather-app">
-          <form class="search-form" id="search-form">
+          <form onSubmit={handleSubmit}
+          class="search-form" id="search-form">
             <div class="form-row">
               <div class="col-5">
                 <input
@@ -36,6 +53,7 @@ return (
                   class="form-control"
                   placeholder="Where?"
                   id="city-input"
+                  onChange={handleCityChange}
               
                 />
               </div>
@@ -58,37 +76,14 @@ return (
             </div>{" "}
           </form>
         </div>
-
-        <div class="row row-cols-3">
-          <div id="city" class="city">{weatherData.city}</div>
-          <div class="today">
-            <img src={weatherData.iconUrl} alt={weatherData.description} id="icon" />
-          </div>
-          <div id="date" class="date"><FormattedDate date={weatherData.date} /></div>
-          <div class="row"></div>
-        </div>
-        <div class="row row-cols-9">
-          <div id="temperature" class="temperature">{Math.round(weatherData.temperature)}</div>
-          <div class="unit">°C </div>
-        </div>
-        <span class="weatherDefinition">{weatherData.description}</span>
-        <ul class="conditions">
-          <li>
-           Humidity: {weatherData.humidity}: <span id="humidity"></span>%
-          </li>
-          <li>
-           Wind: {weatherData.wind} <span id="wind"></span> km/h
-          </li>
-        </ul>
-      </div>
+<WeatherInfo data={weatherData} />
+       
+    </div>
     </div>
   );
  }
   else {
-  const apiKey = "643f59cd53a59997e40900d7df9a1d27";
-  let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(handleResponse);
+  search();
 return "Loading...";
-}
   }
-
+}
--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,6 +1,31 @@
 import React from "react";
+import FormattedDate from "./FormattedDate";
 
 
-export default function WeatherInfo (){
-    return
+export default function WeatherInfo (props){
+    return( <div class="Weatherinfo">
+      <div class="row row-cols-3">
+          <div id="city" class="city">{props.data.city}</div>
+          <div class="today">
+            <img src={props.data.iconUrl} alt={props.data.description} id="icon" />
+          </div>
+          <div id="date" class="date"><FormattedDate date={props.data.date} /></div>
+          <div class="row"></div>
+        </div>
+        <div class="row row-cols-9">
+          <div id="temperature" class="temperature">{Math.round(props.data.temperature)}</div>
+          <div class="unit">°C </div>
+        </div>
+        <span class="weatherDefinition">{props.data.description}</span>
+        <ul class="conditions">
+          <li>
+           Humidity: {props.data.humidity}: <span id="humidity"></span>%
+          </li>
+          <li>
+           Wind: {props.data.wind} <span id="wind"></span> km/h
+          </li>
+        </ul>
+      </div>
+
+    );
 }
