522f8f34f160a73356320041235836bb557f22bc
lenasophielu <72163661+lenasophielu@users.noreply.github.com>
2021-03-08 16:05:00 +0100

    Update Weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -24,7 +24,7 @@ export default function Weather(props) {
   }
 function search()
 { const apiKey = "643f59cd53a59997e40900d7df9a1d27";
-  let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(handleResponse);
 
 }
